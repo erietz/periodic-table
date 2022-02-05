b1d3b42e8b795e0392c2bec7f16f185d982b3f66
Ethan Rietz <ewrietz@gmail.com>
2022-02-04 23:01:42 -0600

    fix confusion

--- periodic-table/src/app/components/table/table.component.ts
+++ periodic-table/src/app/components/table/table.component.ts
@@ -10,22 +10,31 @@ export class TableComponent implements OnInit {
         {
             "col1": "default", 
             "col2": "lkjsdlkjsdflkj", 
-            "col3": "0.02"
+            "col3": "0.02",
+            "col4": "0.02"
         },
         {
             "col1": "sdfsdf", 
             "col2": "sddf", 
-            "col3": "23.34"
+            "col3": "23.34",
+            "col4": "23.34",
         },
         {
             "col1": "lklkj", 
             "col2": "kj2", 
-            "col3": "0888"
+            "col3": "0888",
+            "col4": "23.34",
+        },
+        {
+            "col1": "what", 
+            "col2": "is", 
+            "col3": "happening",
+            "col4": "23.34",
         },
     ]
 
-    // columns = Object.keys(this.table[0]);
-    myCols = ["co1", "col2", "col3"];
+    myCols = Object.keys(this.myTab[0]);
+    // myCols = ["co1", "col2", "col3"];
 
     constructor() { }
 
