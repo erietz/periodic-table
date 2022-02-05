ce67b009c34d854a58160f6ba9e8e46e2b2c14e0
Ethan Rietz <ewrietz@gmail.com>
2022-02-04 21:27:56 -0600

    populate table from json data

--- periodic-table/src/app/components/table/table.component.ts
+++ periodic-table/src/app/components/table/table.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
+    tableArray = [
+        {
+            row1: "test1",
+            row2: "test1",
+            row3: "test1",
+        },
+        {
+            row1: "test2",
+            row2: "test2",
+            row3: "test2",
+        },
+        {
+            row1: "test3",
+            row2: "test3",
+            row3: "test3",
+        },
+    ]
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
