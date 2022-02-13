979f386d3d325e99ddfc56b24c0b99c8fe9d7a13
Ethan Rietz <ewrietz@gmail.com>
2022-02-13 12:10:49 -0600

    refactor: rename variables and fix types

--- periodic-table/src/app/components/table/table.component.ts
+++ periodic-table/src/app/components/table/table.component.ts
@@ -9,66 +9,65 @@ import { COLOR_PALETTE } from 'src/assets/color_palette';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
-    public myTab: {[index: string]: any}[];
-    public myCols: string[];
-    public groups: string[];
-    public palette: {[index: string]: string};
-    public properties: {[index: string]: string};
+  // TODO change any to a better type
+  public table: {[index: string]: any}[];
+  public columns: string[];
+  public groups: string[];
+  public palette: {[index: string]: string};
+  public properties: {[index: string]: string};
 
-    constructor() {
-      [this.myTab, this.groups] = this.loadData();
-      this.myCols = Object.keys(this.myTab[0]);
-      this.palette = this.generateColorPalette();
-      this.properties = this.myTab[0][this.myCols[0]].elementProperties;
-    }
+  constructor() {
+    [this.table, this.groups] = this.loadData();
+    this.columns = Object.keys(this.table[0]);
+    this.palette = this.generateColorPalette();
+    this.properties = this.table[0][this.columns[0]].elementProperties;
+  }
 
-    ngOnInit(): void {
-    }
+  ngOnInit(): void {
+  }
 
-    loadData(): [{[index: string]: any}[], string[]] {
-      let data: {[index: string]: any}[] = [];
-      let groups = new Set<string>();
+  loadData(): [{[index: string]: Cell}[], string[]] {
+    let data: {[index: string]: Cell}[] = [];
+    let groups = new Set<string>();
 
-      for (const row of periodic_data) {
-        let newRow: {[index: string]: Cell} = {};
-        for (const col in row) {
+    for (const row of periodic_data) {
+      let newRow: {[index: string]: Cell} = {};
+      for (const col in row) {
 
-          const d = row[col];
+        const d = row[col];
 
-          newRow[col] = {
-            'elementSymbol': d['Symbol'],
-            'elementName': d['Name'],
-            'elementNumber': d['AtomicNumber'],
-            'elementProperties': d
-          }
-
-          if (d['GroupBlock'] != null && !groups.has(d['GroupBlock'])) {
-            groups.add(d['GroupBlock']);
-          }
-
+        newRow[col] = {
+          'elementSymbol': d['Symbol'],
+          'elementName': d['Name'],
+          'elementNumber': d['AtomicNumber'],
+          'elementProperties': d
         }
-        data.push(newRow);
-      }
 
-      return [data, Array.from(groups)];
-    }
-
-    populateProperties(cell: any): void {
-      this.properties = cell.elementProperties;
-    }
-
-    generateColorPalette(): {[index: string]: string} {
-      const tmpPalette: {[index: string]: string} = {};
-
-      for (let i=0; i<COLOR_PALETTE.length; i++) {
-        if (this.groups[i] != null) {
-          tmpPalette[this.groups[i]] = COLOR_PALETTE[i];
-          console.log(this.groups[i], COLOR_PALETTE[i]);
-          console.log(tmpPalette[this.groups[i]]);
+        if (d['GroupBlock'] != null && !groups.has(d['GroupBlock'])) {
+          groups.add(d['GroupBlock']);
         }
-      }
 
-      return tmpPalette;
+      }
+      data.push(newRow);
     }
+
+    return [data, Array.from(groups)];
+  }
+
+  populateProperties(cell: Cell): void {
+    this.properties = cell.elementProperties;
+  }
+
+  generateColorPalette(): {[index: string]: string} {
+    const tmpPalette: {[index: string]: string} = {};
+
+    for (let i=0; i<COLOR_PALETTE.length; i++) {
+      if (this.groups[i] != null) {
+        tmpPalette[this.groups[i]] = COLOR_PALETTE[i];
+      }
+    }
+
+    return tmpPalette;
+  }
 
 }
