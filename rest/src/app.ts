52aad94dea7cb1de22adf1ad88d172b8c34ea8a2
Ethan Rietz <ewrietz@gmail.com>
2022-02-21 23:02:40 -0600

    definitely not the correct schema

--- rest/src/app.ts
+++ rest/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { getTables, saveTable, Table } from "./model";
 
 const app = express();
 const PORT = 3000;
@@ -7,6 +8,17 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send({ some: "JSON" });
+});
+
+app.get("/tables", (req, res) => {
+    getTables({}, "")
+        .then( (tables: Table[]) => res.json(tables))
+        .catch( err => console.error(err));
+});
+
+app.get("/savetest", async (req, res) => {
+    await saveTable();
+    res.send("Table saved maybe???");
 });
 
 app.listen(PORT, () => {
---
+++ rest/src/model.ts
@@ -0,0 +1,76 @@
+import mongoose from "mongoose";
+
+const db = mongoose.connection;
+// connection string on port 27017 using database named periodicTable
+const connectionURL = "mongodb://localhost:27017/periodicTable";
+
+// connect to the db
+mongoose.connect(connectionURL);
+
+// adds a one timed listener event for the event named "foo"
+db.once("foo", () => {
+    console.log(`Connected to MongoDb using mongoose at ${connectionURL}`);
+});
+
+export interface Table {
+    name: string,
+    data: {
+        [key: string]: {
+            [key: string]: string
+        }
+    }[]
+};
+
+const TableSchema = new mongoose.Schema<Table>({
+    name: { type: String, required: true },
+    data: [
+        {
+            type: {
+                type: String
+            }
+        }
+    ]
+});
+
+const TableModel = mongoose.model<Table>("PeriodicTable", TableSchema);
+
+export async function saveTable(): Promise<any> {
+    const doc = new TableModel({
+        name: "Initial Test Table",
+        data: [
+            {
+                "col1": {
+                    "AtomicNumber":"1",
+                    "Symbol":"H",
+                    "Name":"Hydrogen",
+                    "AtomicMass":"1.0080",
+                    "CPKHexColor":"FFFFFF",
+                    "ElectronConfiguration":"1s1",
+                    "Electronegativity":"2.2",
+                    "AtomicRadius":"120",
+                    "IonizationEnergy":"13.598",
+                    "ElectronAffinity":"0.754",
+                    "OxidationStates":"+1, -1",
+                    "StandardState":"Gas",
+                    "MeltingPoint":"13.81",
+                    "BoilingPoint":"20.28",
+                    "Density":"0.00008988",
+                    "GroupBlock":"Nonmetal",
+                    "YearDiscovered":"1766"
+                },
+                "col2": {},
+                "col3": {},
+                "col4": {}
+            }
+        ]
+    });
+
+    return doc.save();
+}
+
+export async function getTables(filter: object, projection: string): Promise<Table[]> {
+    const query = TableModel.find(filter, projection)
+        .select(projection)
+    return query.exec();
+}
+
